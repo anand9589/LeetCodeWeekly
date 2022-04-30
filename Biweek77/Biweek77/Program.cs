00f2909bd83fcb5842f2f7f7e35df28be1bdbaf9
anand9589 <anand9589@gmail.com>
2022-04-30 10:58:10 -0500

    Added

--- Biweek77/Biweek77/Program.cs
+++ Biweek77/Biweek77/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Biweek77;
+
 Console.WriteLine("Hello, World!");
+Solution solution = new Solution();
+var res = solution.CountUnguarded(4, 6, new int[][] { new int[] { 0, 0 }, new int[] { 1, 1 }, new int[] { 2, 3 } }, new int[][] { new int[] { 0, 1 }, new int[] { 2, 2 }, new int[] { 1, 4 } });
+Console.WriteLine(res);
--- Biweek77/Biweek77/Solution.cs
+++ Biweek77/Biweek77/Solution.cs
@@ -1,6 +1,158 @@
 ﻿namespace Biweek77
 {
-    internal class Solution
+    public class Solution
     {
+        public int CountPrefixes(string[] words, string s)
+        {
+            int result = 0;
+
+            foreach (var w in words)
+            {
+                if (s.StartsWith(w)) result++;
+            }
+            return result;
+        }
+
+        public int MinimumAverageDifference(int[] nums)
+        {
+            long result = int.MaxValue;
+            int index = -1;
+            long sum1 = 0;
+            long sum = nums.Sum(x => (long)x);
+            long sum2 = sum - sum1;
+            long avg1 = 0;
+            long avg2 = 0;
+            long ans = int.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                sum1 += nums[i];
+                sum2 = sum - sum1;
+
+                avg1 = sum1 / (i + 1);
+                avg2 = nums.Length - i - 1 == 0 ? 0 : sum2 / (nums.Length - i - 1);
+
+                ans = Math.Abs(avg1 - avg2);
+                if (ans == 0) return i;
+                if (ans < result)
+                {
+                    result = ans;
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+
+        public int CountUnguarded(int m, int n, int[][] guards, int[][] walls)
+        {
+            int result = 0;
+            int[][] guarded = new int[m][];
+
+            for (int i = 0; i < m; i++)
+            {
+                guarded[i] = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    guarded[i][j] = -1;
+                }
+            }
+
+            for (int i = 0; i < walls.Length; i++)
+            {
+                int x = walls[i][0];
+                int y = walls[i][1];
+
+                guarded[x][y] = 1;
+            }
+
+            for (int i = 0; i < guards.Length; i++)
+            {
+                int x = guards[i][0];
+                int y = guards[i][1];
+
+                guarded[x][y] = 1;
+
+                updateGuarded(guarded, guards, walls, x, y, m, n);
+            }
+
+            for (int i = 0; i < guarded.Length; i++)
+            {
+                for (int j = 0; j < guarded[i].Length; j++)
+                {
+                    if (guarded[i][j] == -1) result++;
+                }
+            }
+
+            return result;
+        }
+
+        private void updateGuarded(int[][] guarded, int[][] guards, int[][] walls, int x, int y, int m, int n)
+        {
+            for (int i = x - 1; i >= 0; i--)
+            {
+                if (isWall(walls, i, y) || isGuard(guards, i, y))
+                {
+                    break;
+                }
+                guarded[i][y] = 1;
+            }
+
+            for (int i = x + 1; i < m; i++)
+            {
+                if (isWall(walls,i,y) || isGuard(guards, i, y))
+                {
+                    break;
+                }
+
+                guarded[i][y] = 1;
+            }
+
+            for (int i = y - 1; i >= 0; i--)
+            {
+
+                if (isWall(walls,x,i) || isGuard(guards, x, i))
+                {
+                    break;
+                }
+
+                guarded[x][i] = 1;
+            }
+
+            for (int i = y + 1; i < n; i++)
+            {
+                if (isWall(walls,x,i) || isGuard(guards, x, i))
+                {
+                    break;
+                }
+
+                guarded[x][i] = 1;
+            }
+        }
+
+        private bool isWall(int[][] walls, int x, int y)
+        {
+            foreach (var w in walls)
+            {
+                if (x == w[0] && y == w[1]) return true;
+            }
+
+            return false;
+        }
+
+        private bool isGuard(int[][] gurads, int x, int y)
+        {
+            foreach (var w in gurads)
+            {
+                if (x == w[0] && y == w[1]) return true;
+            }
+
+            return false;
+        }
+
+        public int MaximumMinutes(int[][] grid)
+        {
+            return -1;
+        }
     }
 }
