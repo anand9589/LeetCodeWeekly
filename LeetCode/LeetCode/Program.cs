15e971750fa64768331c96c6644cf6e436ee57c8
anand9589 <anand9589@gmail.com>
2022-10-02 20:50:16 -0500

    Added weekly 313

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,8 +1,11 @@
-﻿//using October22;
+﻿using October22;
 
-//Solution solution = new Solution();
+Solution solution = new Solution();
+Console.WriteLine(  solution.DeleteString("aaaaa"));
+//solution.MaxSum(new int[][] { new int[] { 6, 2, 1, 3 }, new int[] { 4, 2, 1, 5 }, new int[] { 9, 2, 8, 7 }, new int[] { 4, 1, 2, 9 } });
 
-using LeetCode;
+//solution.MaxSum(new int[][] { new int[] { 520626, 685427, 788912, 800638, 717251, 683428 }, new int[] { 23602, 608915, 697585, 957500, 154778, 209236 }, new int[] { 287585, 588801, 818234, 73530, 939116, 252369 } });
+//using LeetCode;
 
-Problems problems = new Problems();
-problems.MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+//Problems problems = new Problems();
+//problems.MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
--- LeetCode/October22/Solution.cs
+++ LeetCode/October22/Solution.cs
@@ -2,7 +2,7 @@
 {
     public class Solution
     {
-        #region Day 1 Problem 91 Decode Ways
+        #region Day 1 Problem 91. Decode Ways
         public int NumDecodings(string s)
         {
             if (s==null || s.Length == 0 || s.StartsWith('0')) return 0;
@@ -31,6 +31,115 @@
 
             return dp[s.Length];
         }
-        #endregion 
+        #endregion
+
+        #region Day 2 Problem 1155. Number of Dice Rolls With Target Sum
+        public int NumRollsToTarget(int n, int k, int target)
+        {
+            if(target == 0) return 0;
+            if (target > n * k) return 0;
+
+            int count =(int) getRollsToDice(n,k,target);
+
+            
+            return count;
+        }
+
+        private long getRollsToDice(int n, int k, int target)
+        {
+            int mod = (int)Math.Pow(10, 9) + 7;
+            long[][] dp = new long[n+1][];
+            for (int i = 0; i < dp.Length; i++)
+            {
+                dp[i] = new long[target + 1];
+            }
+
+            dp[0][0] = 1;
+
+            for (int dice = 1; dice <= n; dice++)
+            {
+                for (int tar = 1; tar <= target; tar++)
+                {
+                    long count = 0;
+                    for (int i = 1; i <= k; i++)
+                    {
+                        if(tar-i>=0)  count+=dp[dice-1][tar-i];
+                    }
+                    dp[dice][tar] = count;
+                }
+            }
+
+            return dp[n][target]%mod;
+        }
+        #endregion
+
+        #region Weekly 313
+        public int CommonFactors(int a, int b)
+        {
+            int result = 1;
+
+            for (int i = 2; i <= Math.Min(a,b); i++)
+            {
+                if (a % i == 0 && b % i == 0) result++;
+            }
+
+            return result;
+        }
+        public int MaxSum(int[][] grid)
+        {
+            int result = 0;
+
+            for (int i = 1; i < grid.Length-1; i++)
+            {
+                for (int j = 1; j < grid[i].Length-1; j++)
+                {
+                    int sum = grid[i-1][j-1] + grid[i-1][j] + grid[i-1][j+1] + grid[i][j] + grid[i+1][j-1] + grid[i+1][j] +grid[i+1][j+1];
+
+                    result = Math.Max(result, sum); 
+                }
+            }
+
+            return result;
+        }
+
+        public int MinimizeXor(int num1, int num2)
+        {
+            return num1 ^ num2;
+        }
+
+        public int DeleteString(string s)
+        {
+          return  delete(s);
+        }
+
+        private int delete(string s)
+        {
+            if (s.Length == 0)                 return 0;
+            int c = 1;
+
+            string d = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                d += s[i];
+
+                string s2 = "";
+                if (i + d.Length < s.Length)
+                {
+                    s2 = s.Substring(i + 1, d.Length);
+                }
+
+                if (s2 == d)
+                {
+                
+                    c = 1 + delete(s.Substring(i + 1));
+                    break;
+                }
+            }
+
+            return c;
+        }
+        #endregion
     }
 }
