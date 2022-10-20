339fcc2181fba67d3991d61234a8b8e15dc53622
anand9589 <anand9589@gmail.com>
2022-10-19 19:29:39 -0500

    Problem 12. Integer to Roman

--- LeetCode/October22/Solution.cs
+++ LeetCode/October22/Solution.cs
@@ -809,7 +809,39 @@ namespace October22
         }
         #endregion
 
-        #region Day 20 Problem
+        #region Day 20 Problem 12. Integer to Roman
+        public string IntToRoman(int num)
+        {
+            Dictionary<int, string> romanCode = new Dictionary<int, string>()
+            {
+                {1000,"M" },
+                {900,"CM" },
+                {500,"D" },
+                {400,"CD" },
+                {100,"C" },
+                {90,"XC" },
+                {50,"L" },
+                {40,"XL" },
+                {10,"X" },
+                {9,"IX" },
+                {5,"V" },
+                {4,"IV" },
+                {1,"I" }
+            };
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (int item in romanCode.Keys)
+            {
+                while (num>=item)
+                {
+                    sb.Append(romanCode[item]);
+                    num -= item;
+                }
+            }
+
+            return sb.ToString();
+        }
         #endregion
 
         #region Day 21 Problem
