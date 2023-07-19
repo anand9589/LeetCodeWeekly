d4dcc9ceab1a6849fc513306df36ead4efe8a64b
anand9589 <anand9589@gmail.com>
2023-07-18 19:33:02 -0500

    LRUCache

---
+++ LeetCode/Common/DoublyLinkedList.cs
@@ -0,0 +1,30 @@
+﻿namespace Common
+{
+    public class DoublyLinkedList
+    {
+
+        public int Value { get; set; }
+        public int Key { get; private set; }
+        public DoublyLinkedList Previous { get; set; }
+        public DoublyLinkedList Next { get; set; }
+        public DoublyLinkedList()
+        {
+            Key = 0;
+            Value = 0;
+        }
+
+        public DoublyLinkedList(int key, int val)
+        {
+            Key = key;
+            Value = val;
+        }
+
+        public DoublyLinkedList(int value, int key, DoublyLinkedList previous, DoublyLinkedList next)
+        {
+            Value = value;
+            Key = key;
+            Previous = previous;
+            Next = next;
+        }
+    }
+}
